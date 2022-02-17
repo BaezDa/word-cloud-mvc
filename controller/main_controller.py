af8755212466fe52e6c95d0dd73c2f1a969894ce
BaezDa <al061743@uacam.mx>
2022-02-17 11:39:29 -0600

    docuemntacion agregada al controlador

--- controller/main_controller.py
+++ controller/main_controller.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import QFileDialog, QTableWidgetItem
 # from PyQt5.uic import loadUiType
 from pathlib import Path
 
-
+from model.word_cloud import WordCloudI
 
 import pandas as pd
 
@@ -24,7 +24,7 @@ class MainController():
         self.view.spinBox.setValue(10)
 
         #Canvas
-        self.view.mpl_canvas.axes.plot([0,1,2,3,4], [10,1,20,3,40])
+       
         
         #Primer Tab
         self.view.ButtonOpen.clicked.connect(self.open_file)
@@ -37,17 +37,45 @@ class MainController():
         self.path = ""
         
         #Segundo Tab
-        self.view.BtnCloud.clicked.connect(self.show_image)
+        self.view.BtnCloud.clicked.connect(self.show_cloud)
+        
+        #Tercer Tab
+        self.view.BtnGraficar.clicked.connect(self.show_hist)
 
     def open_file(self):
+        """Abre el archivo CSV.
+        
+        Despliega una ventana donde se escoge el archivo CSV.
+        
+        """
         try:
             path = self.view.file_dialog.getOpenFileName(self.view, 'Abrir CSV', os.getenv('HOME'), 'CSV(*.csv)')[0]
             self.all_data = pd.read_csv(path)
             print(path)
         except:
             print(path)
+            
+    def save_file(self):
+        """Abre el archivo CSV.
+        
+        Despliega una ventana donde se escoge el archivo CSV.
+        
+        """
+        try:
+            path = self.view.file_dialog.getSaveFileName(self.view, 'Guardar CSV', os.getenv('HOME'), 'CSV(*.csv)')[0]
+            # self.all_data = pd.read_csv(path)
+            print(path)
+            return path
+        except:
+            print(path)
 
     def data_head(self):
+        """Carga los datos del archivos CSV en TableWidget.
+        
+        Recorre el archivo CSV para asignarlos en las propiedades
+        de un TableWidget.
+        
+        """
         numColomn = self.view.spinBox.value()
         if numColomn == 0:
             NumRows = len(self.all_data.index)
@@ -66,24 +94,54 @@ class MainController():
         self.view.tableWidget.resizeRowsToContents()
 
     def add(self):
+        """Agrega elementos a lista del modelo.
+        
+        Actualiza el modelo cada vez que se agrega un elemento,
+        por lo que require de un emit() para reflejar los cambios
+        del modelo en la interfaz.
+        
+        """
         text = self.view.lineEdit.text()
-        if text:  # Don't add empty strings.
-            # Access the list via the model.
+        if text:  # No se agregan cadenas vacias.
             self.model.todos.append(text)
-            # Trigger refresh.
             self.model.layoutChanged.emit()
-            # Empty the input
             self.view.lineEdit.setText("")
     
     def reduce_db(self):
+        """Crea una base de datos reducida.
+        
+        Obtiene las palabras clave del modelo para filtrar los datos,
+        esto con la finalidad de crear el archivo csv reducido.
+        """
         actors = self.model.get_data()
         print(actors)
         db_reducida = self.all_data[self.all_data['Texto'].str.contains('|'.join(actors))]
-       
-        filepath = Path('C:/Users/Baez/Documents/UAC/8vo semestre/Topicos de lenguajes de programación/Primer parcial/Programa 1 Python/word-cloud-mvc/out.csv')  
+        
+        filepath = Path(self.save_file())  
         db_reducida.to_csv(filepath) 
 
-    def show_image(self):
-        self.view.mpl_canvas.setParent(self.view.frameNube)
-        self.view.mpl_canvas.show()
+    def show_cloud(self):
+        """Muestra la nube de palabras.
         
+        Agrega la imagen generada de la nube de palabras a su contenedor canvas
+        correspondiente.
+        """
+        text =  " ".join(review for review in self.all_data['Texto'])
+        self.view.mpl_canvas_cloud.axes.imshow(WordCloudI(text).generate_word_cloud(), interpolation='bilinear')
+        self.view.mpl_canvas_cloud.axes.axis('off')
+        self.view.mpl_canvas_cloud.setParent(self.view.frameNube)
+        self.view.mpl_canvas_cloud.show()
+    
+    def show_hist(self):
+        """Muestra el histograma.
+        
+        Agrega la imagen generada del histograma a su contenedor canvas
+        correspondiente.
+        """
+        text =  " ".join(review for review in self.all_data['Texto'])
+        words = WordCloudI(text, max_words=20).generate_word_cloud()
+        kwargs = dict(alpha=0.5, bins=100, edgecolor = 'black',  linewidth=1, color='#F2AB6D')
+        self.view.mpl_canvas_hist.axes.bar(list(words.words_.keys()),words.words_.values())
+        self.view.mpl_canvas_hist.axes.tick_params(axis="x", labelrotation=90)
+        self.view.mpl_canvas_hist.setParent(self.view.frameHist)
+        self.view.mpl_canvas_hist.show()
---
+++ model/word_cloud.py
@@ -0,0 +1,17 @@
+from wordcloud import WordCloud
+
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+import matplotlib.pyplot as plt
+
+class WordCloudI():
+    def __init__(self, text, max_words = 40):
+        self.text = text
+        self.max_words = max_words
+        self.stop_words = set(stopwords.words('spanish'))
+        self.stop_words.update(["t", "co", "https", "t co", "https://t.co/", "pack", "LunesDeGanarSeguidores FelizLunes", "SabadoDeGanarSeguidores FelizSabado", "MiercolesDeGanarSeguidores FelizMiercoles", "DomingoDeGanarSeguidores FelizDomingo", "ViernesDeGanarSeguidores FelizViernes", "ven", "Feliz lunes", "persona", "Premium up", "quiero", "FelizDomingo"])
+    
+    def generate_word_cloud(self):
+        return WordCloud(background_color='white', stopwords=self.stop_words, max_words=self.max_words).generate(self.text)
